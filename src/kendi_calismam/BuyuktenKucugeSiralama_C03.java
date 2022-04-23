3d611f71b376b1ff9477eb27db37596b9fbf19eb
Saliha Ozturk <so2022uk@gmail.com>
2022-04-23 18:34:47 +0100

    ilk

---
+++ src/kendi_calismam/BuyuktenKucugeSiralama_C03.java
@@ -0,0 +1,42 @@
+package kendi_calismam;
+
+import java.util.Arrays;
+
+public class BuyuktenKucugeSiralama_C03 {
+
+    public static void main(String[] args) {
+
+        //verilen bir array i buyukten kucuge siralayip,yazdiran bir method olusturun.
+
+        int arr[]={3,5,6,1,9,45,25,4,9,0};
+        terstenSiralaYazdiryazdir(arr);
+
+
+
+
+
+    }
+
+    private static void terstenSiralaYazdiryazdir(int[] arr) {
+
+
+
+        Arrays.sort(arr);
+
+        int tersArr[] = new int[arr.length];
+
+
+        for (int i = 0; i < arr.length; i++) ;
+
+     //   tersArr[i] = arr[arr.length - 1 - i];
+
+    }
+
+
+      // System.out.println(Arrays.toString(tersArr));
+
+
+
+
+
+    }
